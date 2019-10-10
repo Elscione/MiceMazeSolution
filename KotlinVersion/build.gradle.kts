1e6d5d909794006d815bd76832d74393e3aa31e6
Yusuf Hendrawan <yusuf.hendrawan@tokopedia.com>
2019-10-10 14:14:32 +0700

    Adding some comments

--- KotlinVersion/build.gradle.kts
+++ KotlinVersion/build.gradle.kts
@@ -4,26 +4,31 @@ import solution.tasktypes.serial.SerialSolutionTask
 
 val output = File("${buildDir}/outputs")
 val files: FileCollection = fileTree("$rootDir/inputs").matching { include { it.name.startsWith("input") } }.filter { it.isFile }
-output.mkdirs()
 
-// creating serial solution
+tasks.register<Delete>("clean") {
+    delete("build")
+}
+
+// create a single task to solve the solution serially
 tasks.register<SerialSolutionTask>("solutionTaskInSerial") {
     inputFiles.addAll(files)
     outputDirectory = output
 }
 
-// creating parallel solution with single task
+// create a single task that solve the solution in parallel
 tasks.register<ParallelTaskSolution>("solutionSingleTaskParallel") {
     inputFiles.addAll(files)
     outputDirectory = output
     outputDirectory.mkdirs()
 }
 
-// creating parallel solution with multiple task
-fileTree("${project.rootDir}/inputs").files.forEach {
-    tasks.register<ParallelMultiTaskSolution>("${it.name}Processor", it, File("${project.buildDir}/outputs/${it.name.replace("input", "output")}"))
+// create a single task for each input files and assign the work to a worker executor so that we can run multiple task at once
+fileTree("${rootProject.rootDir}/inputs").files.forEach {
+    tasks.register<ParallelMultiTaskSolution>(it.name, it, File("${project.buildDir}/outputs/${it.name.replace("input", "output")}"))
 }
 
+// create a task that depends on input tasks created above
+// so that we can run all the input task by executing this task
 tasks.register("multiTask") {
-    dependsOn(tasks.matching { it.name.endsWith("Processor") })
+    dependsOn(tasks.matching { it.name.startsWith("input") })
 }
